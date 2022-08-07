8fbfc73fc86bff25a8963913daa31a63570b0ab1
ultddeveloper <andrew.mutien@gmail.com>
2022-08-07 17:01:46 +0800

    Implement Collections & Add Title for Search

--- src/templateData/navigation.ts
+++ src/templateData/navigation.ts
@@ -6,3 +6,17 @@ export const SampleSearchResults = [
     SampleArticle,
     SampleArticle,
 ];
+
+export const SampleCollections = [
+    { date: "2022-01-01", collections: [SampleArticle] },
+    {
+        date: "2022-01-01",
+        collections: [
+            SampleArticle,
+            SampleArticle,
+            SampleArticle,
+            SampleArticle,
+        ],
+    },
+    { date: "2022-01-01", collections: [SampleArticle] },
+];
