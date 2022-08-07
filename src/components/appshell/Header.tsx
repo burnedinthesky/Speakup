eca3d33b48c2bb703381f8572969534d69d2d3c0
ultddeveloper <andrew.mutien@gmail.com>
2022-08-07 16:37:21 +0800

    Implement Search Box & Page

--- src/components/appshell/Header.tsx
+++ src/components/appshell/Header.tsx
@@ -1,24 +1,9 @@
-import { SyntheticEvent, useState } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
-
-import { SearchIcon } from "@heroicons/react/outline";
-
 // import DesktopNotifications from "./header/DesktopNotificationHandler";
 import AccountOptions from "../Account/AccountOptions";
+import DesktopDropdown from "../Navigation/Search/DesktopDropdown";
 
 const Header = () => {
-    const [searchKeyword, setSearchKeyword] = useState<string>("");
-
-    const router = useRouter();
-
-    const searchSubmit = (e: SyntheticEvent) => {
-        e.preventDefault();
-        if (searchKeyword !== null) {
-            router.push(`/search/results?searchterm=${searchKeyword}`);
-        }
-    };
-
     return (
         <div className="fixed top-0 z-20 h-14 w-screen bg-primary-700 px-6 xl:px-14">
             <div className="flex h-full cursor-pointer items-center lg:hidden">
@@ -40,23 +25,9 @@ const Header = () => {
                             alt="logo"
                         />
                     </Link>
-                    <form
-                        className="hidden w-7/12 max-w-2xl items-center md:flex xl:w-5/12"
-                        onSubmit={searchSubmit}
-                    >
-                        <input
-                            value={searchKeyword}
-                            onChange={(e) => {
-                                setSearchKeyword(e.target.value);
-                            }}
-                            className="h-9 w-full rounded-3xl bg-neutral-50 p-5 text-base text-neutral-600 focus:outline-0"
-                            placeholder="搜尋你感興趣的議題"
-                            type="text"
-                        />
-                        <button type="submit" className="relative -left-12">
-                            <SearchIcon className="h-6 w-6 text-primary-800" />
-                        </button>
-                    </form>
+                    <div className=" w-7/12 max-w-2xl  xl:w-5/12">
+                        <DesktopDropdown />
+                    </div>
                 </div>
                 <div className="flex h-9 items-center justify-end gap-5 ">
                     {/* <DesktopNotifications /> */}
---
+++ src/pages/search/index.tsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from "react";
+import { ClipboardCopyIcon, SearchIcon } from "@heroicons/react/outline";
+import Header from "../../components/AppShell/Header";
+import Navbar from "../../components/AppShell/Navbar";
+import Footbar from "../../components/AppShell/Footbar";
+import { ArticleTagValues } from "../../types/issueTypes";
+import { EduTagIcon } from "../../components/TagIcons";
+import { TextInput } from "@mantine/core";
+
+const SearchMenu = () => {
+    const [searchText, setSearchText] = useState("");
+
+    useEffect(() => {
+        if (!localStorage.getItem("AuthToken")) {
+            window.location.href = "/login";
+        }
+    }, []);
+
+    const tags = ArticleTagValues;
+
+    const formSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (searchText !== "") {
+            window.location.href = `/search/results?searchterm=${searchText}`;
+        }
+    };
+
+    return (
+        <div className="fixed left-0 top-0 h-screen w-screen bg-neutral-50 lg:bg-neutral-100">
+            <Header />
+            <Navbar retractable={false} />
+            <Footbar />
+            <div className="lg:ml-64">
+                <div className="mx-auto mt-14 max-w-3xl px-8 py-5 md:mt-[20vh] lg:rounded-2xl">
+                    <form
+                        className="relative flex h-8 w-full items-center lg:h-14"
+                        onSubmit={formSubmit}
+                    >
+                        <TextInput
+                            className="w-full "
+                            classNames={{
+                                input: "h-10 rounded-2xl lg:h-16 lg:text-lg",
+                            }}
+                            placeholder="搜尋議題名字或是#標籤"
+                            value={searchText}
+                            onChange={(e) => {
+                                setSearchText(e.target.value);
+                            }}
+                            rightSection={
+                                <button className=" h-5" type="submit">
+                                    <SearchIcon className="h-full w-full" />
+                                </button>
+                            }
+                        />
+                    </form>
+                    <div className="mx-auto my-7 w-full text-primary-800">
+                        <h2 className="my-3 text-xl lg:my-4 lg:text-2xl">
+                            #推薦標籤
+                        </h2>
+                        <div className="flex flex-wrap gap-x-4 gap-y-5">
+                            {tags.map((tag, i) => (
+                                <button
+                                    key={i}
+                                    className=" bg-white-100 flex gap-2 rounded-lg border-[1px] border-primary-700 bg-white px-2 py-1"
+                                    onClick={() => {
+                                        window.location.href = `/search/results?searchterm=@${tag}`;
+                                    }}
+                                >
+                                    <ClipboardCopyIcon className="h-6 w-6" />
+                                    <p className="my-auto text-sm lg:text-lg">
+                                        {tag}
+                                    </p>
+                                </button>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SearchMenu;
