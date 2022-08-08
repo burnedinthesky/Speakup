12c35989c1f7ee591d21ba07402b866d8b51dc7a
yeelaine29 <yeelainehou@gmail.com>
2022-08-09 01:52:21 +0800

    Discussion Schema Update

---
+++ prisma/migrations/20220808172551_init/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "Comments" ALTER COLUMN "dislikedCount" SET DEFAULT 0,
+ALTER COLUMN "likedCount" SET DEFAULT 0,
+ALTER COLUMN "supportedCount" SET DEFAULT 0;
---
+++ prisma/migrations/20220808172626_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Comments" ALTER COLUMN "commentCount" SET DEFAULT 0;
