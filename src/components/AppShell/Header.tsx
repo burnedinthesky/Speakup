0dfe54d6b68a47e644dee0534be434c9e8a605bd
ultddeveloper <andrew.mutien@gmail.com>
2022-08-13 16:24:01 +0800

    Disable Git Ignore Case

---
+++ src/components/AppShell/Header.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+// import DesktopNotifications from "./header/DesktopNotificationHandler";
+import AccountOptions from "../Account/AccountOptions";
+import DesktopDropdown from "../Navigation/Search/DesktopDropdown";
+
+const Header = () => {
+    return (
+        <div className="fixed top-0 z-20 h-14 w-screen bg-primary-700 px-6 xl:px-14">
+            <div className="flex h-full cursor-pointer items-center lg:hidden">
+                <Link href="/home">
+                    <img
+                        className="my-auto h-10 cursor-pointer"
+                        src="/assets/logo-white.svg"
+                        alt="logo"
+                    />
+                </Link>
+            </div>
+
+            <div className="hidden items-center justify-between lg:flex">
+                <div className="flex h-14 w-screen items-center gap-14">
+                    <Link href="/home">
+                        <img
+                            className="my-auto h-10"
+                            src="/assets/logo-white.svg"
+                            alt="logo"
+                        />
+                    </Link>
+                    <div className=" w-7/12 max-w-2xl  xl:w-5/12">
+                        <DesktopDropdown />
+                    </div>
+                </div>
+                <div className="flex h-9 items-center justify-end gap-5 ">
+                    {/* <DesktopNotifications /> */}
+                    <AccountOptions />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/AppShell/Navbar.tsx
@@ -0,0 +1,104 @@
+import React, { ReactElement } from "react";
+import Link from "next/link";
+
+import {
+    HomeIcon,
+    TrendingUpIcon,
+    BookmarkIcon,
+} from "@heroicons/react/outline";
+import { ArticleTagValues } from "../../types/issueTypes";
+
+//prettier-ignore
+const tags = ArticleTagValues;
+
+interface NavbarSectionProps {
+    retractable: boolean;
+    icon: ReactElement;
+    link: string;
+    text: string;
+}
+
+const NavbarSection = ({
+    retractable,
+    link,
+    icon,
+    text,
+}: NavbarSectionProps) => {
+    return (
+        <Link href={link}>
+            <li className="flex cursor-pointer list-none gap-4 py-3">
+                {icon}
+                <p
+                    className={`whitespace-nowrap text-xl leading-8 ${
+                        retractable
+                            ? "text-transparent group-hover:text-primary-900"
+                            : "text-primary-900"
+                    } `}
+                >
+                    {text}
+                </p>
+            </li>
+        </Link>
+    );
+};
+
+const Navbar = ({ retractable }: { retractable: boolean }) => {
+    return (
+        <nav
+            className={`transition-width group fixed top-14 left-0 z-10 hidden h-[calc(100vh-56px)] flex-shrink-0 flex-col overflow-hidden overflow-x-hidden rounded-r-[32px] bg-neutral-50 pt-6 duration-500 ease-out lg:flex ${
+                retractable
+                    ? "w-20 hover:w-64 hover:drop-shadow-xl lg:flex"
+                    : "w-64"
+            }`}
+        >
+            <div className="h-44 w-full">
+                <ul className="mx-auto pl-7 text-primary-900">
+                    <NavbarSection
+                        retractable={retractable}
+                        link="/home"
+                        icon={<HomeIcon className="w-7 flex-shrink-0" />}
+                        text="首頁"
+                    />
+                    <NavbarSection
+                        retractable={retractable}
+                        link="/search/results?searchterm=@熱門議題"
+                        icon={<TrendingUpIcon className="w-7 flex-shrink-0" />}
+                        text="熱門議題"
+                    />
+                    <NavbarSection
+                        retractable={retractable}
+                        link="/collections"
+                        icon={<BookmarkIcon className="w-7 flex-shrink-0" />}
+                        text="我的收藏"
+                    />
+                </ul>
+            </div>
+
+            <hr className="my-3 mx-auto w-5/6 border-t-2 border-gray-300" />
+
+            <div
+                className={`flex-grow overflow-y-auto pb-6 ${
+                    retractable ? "hidden group-hover:block" : "block"
+                }`}
+            >
+                <ul className="h-full list-none overflow-x-hidden pl-20">
+                    {tags.map((tag, i) => (
+                        <Link
+                            href={`/search/results?searchterm=@${tag}`}
+                            key={`link${i}`}
+                        >
+                            <li
+                                key={i}
+                                className="cursor-pointer whitespace-nowrap py-2 text-lg text-primary-900 "
+                            >
+                                {tag}
+                            </li>
+                        </Link>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
