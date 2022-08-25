1f7e9b012c0987330ffa2444b3dbba84b4d0201f
ultddeveloper <andrew.mutien@gmail.com>
2022-08-25 17:10:13 +0800

    Refactor Selectors

--- src/pages/discussion/[boardId].tsx
+++ src/pages/discussion/[boardId].tsx
@@ -1,17 +1,19 @@
 import { MouseEvent, useEffect, useState } from "react";
-import Head from "next/head";
-import { ActionIcon, Menu, Tabs } from "@mantine/core";
+import { GetServerSideProps } from "next";
+import { supabase } from "../../utils/supabaseClient";
 
-import Header from "../../components/AppShell/Header";
-import Navbar from "../../components/AppShell/Navbar";
-import Footbar from "../../components/AppShell/Footbar";
+import Head from "next/head";
+
+import { Header, Navbar, Footbar } from "../../components/AppShell";
 import ArticleViewer from "../../components/Discussion/Article/ArticleViewer";
 import CommentField from "../../components/Discussion/Comments/CommentField";
+
+import StanceSelector from "../../components/Discussion/Selectors/StanceSelector";
+import SortSelector from "../../components/Discussion/Selectors/SortSelector";
 
 import { Article } from "../../types/issueTypes";
 import { Stances } from "../../types/commentTypes";
 import { SampleArticle } from "../../templateData/issues";
-import { ChevronDownIcon, SortDescendingIcon } from "@heroicons/react/outline";
 
 interface DiscussionProps {
     article: Article;
@@ -67,90 +69,17 @@ const DiscussionBoard = ({ article }: DiscussionProps) => {
                         <div className="h-10" />
                         <div className="mx-auto flex w-full justify-between lg:mb-4">
                             <div className="w-full bg-neutral-50 pb-1 pt-0.5 lg:w-1/2 lg:min-w-[360px] lg:rounded-full lg:px-8">
-                                <Tabs
-                                    color="primary"
-                                    value={viewingStance}
-                                    variant={
-                                        screenSize == "mob"
-                                            ? "outline"
-                                            : "default"
-                                    }
-                                    onTabChange={setViewingStance}
-                                >
-                                    <Tabs.List position="center" grow>
-                                        <Tabs.Tab value="sup">支持</Tabs.Tab>
-                                        <Tabs.Tab value="both">
-                                            無區分立場
-                                        </Tabs.Tab>
-                                        <Tabs.Tab value="agn">反對</Tabs.Tab>
-                                        {screenSize == "mob" && (
-                                            <Menu
-                                                classNames={{
-                                                    dropdown:
-                                                        "bg-neutral-50 shadow-lg",
-                                                }}
-                                                position="bottom-end"
-                                            >
-                                                <Menu.Target>
-                                                    <ActionIcon className="my-[12px] mr-4">
-                                                        <SortDescendingIcon className="h-5 w-5" />
-                                                    </ActionIcon>
-                                                </Menu.Target>
-                                                <Menu.Dropdown>
-                                                    <Menu.Item
-                                                        onClick={
-                                                            updateSortMethod
-                                                        }
-                                                    >
-                                                        預設排序
-                                                    </Menu.Item>
-                                                    <Menu.Item
-                                                        onClick={
-                                                            updateSortMethod
-                                                        }
-                                                    >
-                                                        依時間排序
-                                                    </Menu.Item>
-                                                    <Menu.Item
-                                                        onClick={
-                                                            updateSortMethod
-                                                        }
-                                                    >
-                                                        依回覆數排序
-                                                    </Menu.Item>
-                                                </Menu.Dropdown>
-                                            </Menu>
-                                        )}
-                                    </Tabs.List>
-                                </Tabs>
+                                <StanceSelector
+                                    viewingStance={viewingStance}
+                                    screenSize={screenSize}
+                                    setViewingStance={setViewingStance}
+                                    updateSortMethod={updateSortMethod}
+                                />
                             </div>
                             <div className="hidden lg:block">
-                                <Menu
-                                    classNames={{
-                                        dropdown: "bg-neutral-50 shadow-lg",
-                                    }}
-                                    position="bottom-end"
-                                >
-                                    <Menu.Target>
-                                        <button className="flex h-[42px] items-center gap-2 rounded-full bg-neutral-50 px-4">
-                                            <ChevronDownIcon className="h-5 w-5" />
-                                            <p className="text-sm">
-                                                選擇排序方式
-                                            </p>
-                                        </button>
-                                    </Menu.Target>
-                                    <Menu.Dropdown>
-                                        <Menu.Item onClick={updateSortMethod}>
-                                            預設排序
-                                        </Menu.Item>
-                                        <Menu.Item onClick={updateSortMethod}>
-                                            依時間排序
-                                        </Menu.Item>
-                                        <Menu.Item onClick={updateSortMethod}>
-                                            依回覆數排序
-                                        </Menu.Item>
-                                    </Menu.Dropdown>
-                                </Menu>
+                                <SortSelector
+                                    updateSortMethod={updateSortMethod}
+                                />
                             </div>
                         </div>
                         <CommentField
@@ -168,11 +97,30 @@ const DiscussionBoard = ({ article }: DiscussionProps) => {
 
 export default DiscussionBoard;
 
-export async function getServerSideProps() {
-    // const res = await fetch(
-    //     `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/user/home`
-    // );
-    // const data = await res.json();
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const boardId = context?.params?.boardId as string;
 
-    return { props: { article: SampleArticle } };
-}
+    const { data, error } = await supabase
+        .from("Articles")
+        .select(
+            ` 
+        author (
+            id
+        )`
+        )
+        .eq("id", boardId)
+        // .limit(1)
+        .single();
+    if (data?.length == 0) {
+        return {
+            notFound: true,
+        };
+    }
+
+    console.log("Data");
+    console.log(data);
+
+    return {
+        props: { article: SampleArticle },
+    };
+};
