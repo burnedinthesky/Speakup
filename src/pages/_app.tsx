71059f0ef7f7ef48ab2da27268018ecf136968c3
ultddeveloper <andrew.mutien@gmail.com>
2022-10-26 20:59:14 +0800

    Clean WithTRPC

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 // src/pages/_app.tsx
 import type { AppRouter } from "../server/router/app.router";
 import type { AppProps } from "next/app";
+import type { Session } from "next-auth";
 
 import { SessionProvider } from "next-auth/react";
 
@@ -12,7 +13,11 @@ import superjson from "superjson";
 
 import "../styles/globals.css";
 
-const Speakup = ({ Component, pageProps }: AppProps) => {
+interface AppPropsInterface {
+    session: Session | null | undefined;
+}
+
+const Speakup = ({ Component, pageProps }: AppProps<AppPropsInterface>) => {
     return (
         <SessionProvider session={pageProps.session}>
             <MantineProvider
@@ -59,7 +64,6 @@ const getBaseUrl = () => {
     if (typeof window !== "undefined") {
         return "";
     }
-    if (process.browser) return ""; // Browser should use current path
     if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
 
     return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
@@ -67,23 +71,13 @@ const getBaseUrl = () => {
 
 export default withTRPC<AppRouter>({
     config({ ctx }) {
-        /**
-         * If you want to use SSR, you need to use the server's full URL
-         * @link https://trpc.io/docs/ssr
-         */
         const url = `${getBaseUrl()}/api/trpc`;
 
         return {
             url,
             transformer: superjson,
-            /**
-             * @link https://react-query.tanstack.com/reference/QueryClient
-             */
-            // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+            queryClientConfig: {}, //https://react-query.tanstack.com/reference/QueryClient
         };
     },
-    /**
-     * @link https://trpc.io/docs/ssr
-     */
-    ssr: false,
+    ssr: false, // https://trpc.io/docs/ssr
 })(Speakup);
