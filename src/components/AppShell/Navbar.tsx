dd508b952ed56e639fc9b1fcc633e4acae261915
ultddeveloper <andrew.mutien@gmail.com>
2022-10-31 17:42:33 +0800

    Restore Middleware & Implement Canny Message

--- src/components/AppShell/Navbar.tsx
+++ src/components/AppShell/Navbar.tsx
@@ -5,6 +5,7 @@ import {
     HomeIcon,
     TrendingUpIcon,
     BookmarkIcon,
+    ShieldExclamationIcon,
 } from "@heroicons/react/outline";
 import { ArticleTagValues } from "../../types/article.types";
 import { ScrollArea } from "@mantine/core";
@@ -46,60 +47,86 @@ const NavbarSection = ({
 const Navbar = ({ retractable }: { retractable: boolean }) => {
     return (
         <nav
-            className={`group fixed top-14 left-0 z-10 hidden h-[calc(100vh-56px)] flex-shrink-0 flex-col overflow-hidden rounded-r-[32px] bg-neutral-50 pt-6 transition-width duration-500 ease-out overflow-x-hidden lg:flex ${
+            className={`group fixed top-14 left-0 z-10 hidden h-[calc(100vh-56px)] flex-shrink-0 rounded-r-md bg-neutral-50 transition-width duration-500 ease-out lg:flex ${
                 retractable
                     ? "w-20 hover:w-64 hover:drop-shadow-xl lg:flex"
                     : "w-64"
             }`}
         >
-            <div className="h-44 w-full">
-                <ul className="mx-auto pl-7 text-primary-900">
-                    <NavbarSection
-                        retractable={retractable}
-                        link="/home"
-                        icon={<HomeIcon className="w-7 flex-shrink-0" />}
-                        text="首頁"
-                    />
-                    <NavbarSection
-                        retractable={retractable}
-                        link="/search/results?tags=熱門議題"
-                        icon={<TrendingUpIcon className="w-7 flex-shrink-0" />}
-                        text="熱門議題"
-                    />
-                    <NavbarSection
-                        retractable={retractable}
-                        link="/collections"
-                        icon={<BookmarkIcon className="w-7 flex-shrink-0" />}
-                        text="我的收藏"
-                    />
-                </ul>
-            </div>
-
-            <hr className="my-3 mx-auto w-5/6 border-t-2 border-gray-300" />
-
-            <ScrollArea>
-                <div
-                    className={`flex-grow pb-6 ${
-                        retractable ? "hidden group-hover:block" : "block"
-                    }`}
-                >
-                    <ul className="list-none pl-20 overflow-x-hidden">
-                        {tags.map((tag, i) => (
-                            <Link
-                                href={`/search/results?tags=${tag}`}
-                                key={`link${i}`}
-                            >
-                                <li
-                                    key={i}
-                                    className="cursor-pointer whitespace-nowrap py-2 text-lg text-primary-900 "
-                                >
-                                    {tag}
-                                </li>
-                            </Link>
-                        ))}
+            <div className="relative flex h-full w-full flex-col overflow-hidden pt-6">
+                <div className="h-44 w-full">
+                    <ul className="mx-auto pl-7 text-primary-900">
+                        <NavbarSection
+                            retractable={retractable}
+                            link="/home"
+                            icon={<HomeIcon className="w-7 flex-shrink-0" />}
+                            text="首頁"
+                        />
+                        <NavbarSection
+                            retractable={retractable}
+                            link="/search/results?tags=熱門議題"
+                            icon={
+                                <TrendingUpIcon className="w-7 flex-shrink-0" />
+                            }
+                            text="熱門議題"
+                        />
+                        <NavbarSection
+                            retractable={retractable}
+                            link="/collections"
+                            icon={
+                                <BookmarkIcon className="w-7 flex-shrink-0" />
+                            }
+                            text="我的收藏"
+                        />
                     </ul>
                 </div>
-            </ScrollArea>
+
+                <hr className="my-3 mx-auto w-5/6 border-t-2 border-gray-300" />
+
+                <ScrollArea className="h-[calc(100vh-360px)]">
+                    <div
+                        className={`flex-grow pb-6  ${
+                            retractable ? "hidden group-hover:block" : "block"
+                        }`}
+                    >
+                        <ul className="list-none pl-20 overflow-x-hidden">
+                            {tags.map((tag, i) => (
+                                <Link
+                                    href={`/search/results?tags=${tag}`}
+                                    key={`link${i}`}
+                                >
+                                    <li
+                                        key={i}
+                                        className="cursor-pointer whitespace-nowrap py-2 text-lg text-primary-900 "
+                                    >
+                                        {tag}
+                                    </li>
+                                </Link>
+                            ))}
+                        </ul>
+                    </div>
+                </ScrollArea>
+
+                <div className="absolute bottom-0 left-0 flex h-20 w-full items-center gap-4 px-7">
+                    <ShieldExclamationIcon className="h-6 w-6 flex-shrink-0" />
+                    <div
+                        className={`${
+                            retractable ? "hidden" : "block"
+                        } group-hover:block`}
+                    >
+                        <a
+                            href="https://speakup.canny.io/bugs"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <p className="text-sm text-neutral-700">
+                                Speakup目前正在測試階段，請到 Canny
+                                回報錯誤，協助我們改進
+                            </p>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </nav>
     );
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,37 +3,37 @@ import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
-    // const token = await getToken({ req: request });
-    // if (!token)
-    //     return NextResponse.redirect(new URL("/user/signin", request.url));
-    // let rootURL = "";
-    // let slashCounter = 0;
-    // for (let i = 0; i < request.url.length && slashCounter < 3; i++) {
-    //     rootURL += request.url[i];
-    //     if (request.url[i] == "/") slashCounter += 1;
-    // }
-    // rootURL = rootURL.substring(0, rootURL.length - 1);
-    // const URLPath = request.url.substring(rootURL.length, request.url.length);
-    // const res = await fetch(`${rootURL}/api/auth/validateuser`, {
-    //     method: "POST",
-    //     body: JSON.stringify({
-    //         id: token.id,
-    //     }),
-    // });
-    // if (!res.ok)
-    //     return NextResponse.redirect(new URL("/user/signin", request.url));
-    // const valData = await res.json();
-    // if (res.status === 201) {
-    //     if (!valData.Token)
-    //         return NextResponse.redirect(new URL(`/user/signup`, request.url));
-    //     return NextResponse.redirect(
-    //         new URL(`/user/signup?token=${valData.Token}`, request.url)
-    //     );
-    // }
-    // if (valData.Message === "Onboard" && URLPath !== "/user/onboarding")
-    //     return NextResponse.redirect(new URL(`/user/onboarding`, request.url));
-    // else if (valData.Message !== "Onboard" && URLPath === "/user/onboarding")
-    //     return NextResponse.redirect(new URL(`/home`, request.url));
+    const token = await getToken({ req: request });
+    if (!token)
+        return NextResponse.redirect(new URL("/user/signin", request.url));
+    let rootURL = "";
+    let slashCounter = 0;
+    for (let i = 0; i < request.url.length && slashCounter < 3; i++) {
+        rootURL += request.url[i];
+        if (request.url[i] == "/") slashCounter += 1;
+    }
+    rootURL = rootURL.substring(0, rootURL.length - 1);
+    const URLPath = request.url.substring(rootURL.length, request.url.length);
+    const res = await fetch(`${rootURL}/api/auth/validateuser`, {
+        method: "POST",
+        body: JSON.stringify({
+            id: token.id,
+        }),
+    });
+    if (!res.ok)
+        return NextResponse.redirect(new URL("/user/signin", request.url));
+    const valData = await res.json();
+    if (res.status === 201) {
+        if (!valData.Token)
+            return NextResponse.redirect(new URL(`/user/signup`, request.url));
+        return NextResponse.redirect(
+            new URL(`/user/signup?token=${valData.Token}`, request.url)
+        );
+    }
+    if (valData.Message === "Onboard" && URLPath !== "/user/onboarding")
+        return NextResponse.redirect(new URL(`/user/onboarding`, request.url));
+    else if (valData.Message !== "Onboard" && URLPath === "/user/onboarding")
+        return NextResponse.redirect(new URL(`/home`, request.url));
 }
 
 export const config = {
