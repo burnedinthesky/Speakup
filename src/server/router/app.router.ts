d567416bd6e4289b68a6da44396e40f9820c7acb
ultddeveloper <andrew.mutien@gmail.com>
2022-11-06 20:10:34 +0800

    Implement IP Test

--- src/server/router/app.router.ts
+++ src/server/router/app.router.ts
@@ -5,8 +5,18 @@ import { commentsRouter } from "./comments.router";
 import { navigationRouter } from "./navigation.router";
 import { reportRouter } from "./report.router";
 import { userRouter } from "./user.router";
+import requestIp from "request-ip";
 
 export const appRouter = createRouter()
+    .middleware(async ({ ctx, next }) => {
+        // console.log(ctx.req);
+        await ctx.prisma.articleViews.create({
+            data: {
+                ip: requestIp.getClientIp(ctx.req),
+            },
+        });
+        return next();
+    })
     .merge("articles.", articleRouter)
     .merge("arguments.", argumentsRouter)
     .merge("comments.", commentsRouter)
