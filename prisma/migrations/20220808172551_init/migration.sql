a4883a4bf2af3c2072cc111e8b3f82e7572fc949
ultddeveloper <andrew.mutien@gmail.com>
2022-11-13 21:11:57 +0800

    Update scripts & env for production db

--- prisma/migrations/20220808172551_init/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Comments" ALTER COLUMN "dislikedCount" SET DEFAULT 0,
-ALTER COLUMN "likedCount" SET DEFAULT 0,
-ALTER COLUMN "supportedCount" SET DEFAULT 0;
--- prisma/migrations/20220808172626_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Comments" ALTER COLUMN "commentCount" SET DEFAULT 0;
