285e2a16a9a626ea9d17e03cb825c5b1b070f61e
ultddeveloper <andrew.mutien@gmail.com>
2022-11-14 22:30:16 +0800

    Link Advocate Issues to Backend

---
+++ src/pages/advocate/issues/[boardId].tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AppShell } from "../../../components/Advocate/AppShell";
+
+const BoardEditor = () => {
+    return (
+        <AppShell title={`Speakup -`} highlight="issues">
+            <div></div>
+        </AppShell>
+    );
+};
+
+export default BoardEditor;
--- src/types/advocate/article.types.ts
+++ src/types/advocate/article.types.ts
@@ -1,6 +1,10 @@
-import { Article } from "../article.types";
-
-export interface AvcArticle extends Article {
-    status: "passed";
+export interface AvcArticleCard {
+    id: string;
+    title: string;
+    tags: string[];
+    viewCount: number;
+    argumentCount: number;
+    status: "pending_mod" | "passed";
+    status_desc: string;
     modPending: number;
 }
