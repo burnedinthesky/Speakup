aac75ed9c5bae726a7c8f421b41187afba3920c1
ultddeveloper <andrew.mutien@gmail.com>
2022-11-15 23:11:01 +0800

    Implement Link Preview In Article Editor

---
+++ prisma/migrations/20221115085201_made_article_reference_img_optional/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "ArticleReferences" ALTER COLUMN "img" DROP NOT NULL;
--- src/components/Advocate/Issues/Editor/ArticleEditor.tsx
+++ src/components/Advocate/Issues/Editor/ArticleEditor.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { trpc } from "../../../../utils/trpc";
 import { useRouter } from "next/router";
@@ -17,7 +17,9 @@ import { articlePropertiesAtom } from "../../../../atoms/advocate/articleEditorA
 import {
     ArticleBlock,
     ArticleBlockTypes,
+    ReferencesLink,
 } from "../../../../types/article.types";
+import ReferenceCard from "../../../Discussion/Article/ReferenceCard";
 
 const TypeToSize = {
     h1: "text-2xl",
@@ -27,15 +29,37 @@ const TypeToSize = {
     spoiler: "text-base",
 };
 
+function isValidHttpUrl(url: string) {
+    try {
+        new URL(url);
+    } catch (_) {
+        return false;
+    }
+    return true;
+}
+
 interface Errors {
     title: string | null;
     content: string | null;
+    refLinks: string | null;
+}
+
+interface AppendUrlState {
+    text: string;
+    error: string | null;
+}
+
+interface RawRefLinks {
+    status: "queued" | "loading" | "fetched" | "not_found";
+    data: ReferencesLink | null;
+    url: string;
 }
 
 interface ArticleEditorProps {
     articleId?: string;
     initialTitle?: string;
     initialContent?: string[];
+    initialRefLinks?: RawRefLinks[];
     blockStyles: ArticleBlockTypes[];
     setBlockStyles: (
         fn: (cur: ArticleBlockTypes[]) => ArticleBlockTypes[]
@@ -48,6 +72,7 @@ const ArticleEditor = ({
     articleId,
     initialTitle,
     initialContent,
+    initialRefLinks,
     blockStyles,
     setBlockStyles,
     focusBlock,
@@ -61,13 +86,22 @@ const ArticleEditor = ({
     const [articleContent, setArticleContent] = useState<string[]>(
         initialContent ? initialContent : []
     );
+    const [referencesLinks, setReferenceLinks] = useState<RawRefLinks[]>(
+        initialRefLinks ? initialRefLinks : []
+    );
     const [articleProperties, setArticleProperties] = useRecoilState(
         articlePropertiesAtom
     );
 
+    const [addRefLinkInput, setAddRefLinkInput] = useState<AppendUrlState>({
+        text: "",
+        error: null,
+    });
+
     const [errors, setErrors] = useState<Errors>({
         title: null,
         content: null,
+        refLinks: null,
     });
 
     const updateArticleMutation = trpc.useMutation(
@@ -169,7 +203,18 @@ const ArticleEditor = ({
             passed++;
         }
 
-        if (passed < 4) return;
+        if (referencesLinks.length < 2) {
+            setErrors((cur) => ({ ...cur, refLinks: "請參考至少兩篇資料" }));
+        } else {
+            setErrors((cur) => ({ ...cur, refLinks: null }));
+            passed++;
+        }
+
+        if (passed < 5) return;
+
+        console.log(referencesLinks);
+
+        const refUrls: string[] = referencesLinks.map((link) => link.url);
 
         updateArticleMutation.mutate({
             id: articleId,
@@ -183,12 +228,78 @@ const ArticleEditor = ({
                     } as ArticleBlock)
             ),
             tags: articleProperties.tags,
-            references: [],
+            references: refUrls,
         });
     };
 
+    const updateReferenceLinks = () => {
+        referencesLinks.forEach((link) => {
+            if (link.status === "queued") {
+                setReferenceLinks((curLinks) =>
+                    curLinks.map((curLink) =>
+                        curLink.url === link.url
+                            ? {
+                                  ...curLink,
+                                  status: "loading",
+                              }
+                            : curLink
+                    )
+                );
+                fetch("/api/utils/previewlink/", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        link: link.url,
+                    }),
+                })
+                    .then(async (res) => {
+                        if (!res.ok)
+                            throw new Error(
+                                res.status === 404 ? "not_found" : "unknown"
+                            );
+                        const data = await res.json();
+                        setReferenceLinks((curLinks) =>
+                            curLinks.map((ftrLink) =>
+                                ftrLink.url === link.url
+                                    ? {
+                                          data: data,
+                                          status: "fetched",
+                                          url: ftrLink.url,
+                                      }
+                                    : ftrLink
+                            )
+                        );
+                    })
+                    .catch((err) => {
+                        if (err.message === "not_found")
+                            setReferenceLinks((curLinks) =>
+                                curLinks.map((ftrLink) =>
+                                    ftrLink.url === link.url
+                                        ? {
+                                              data: null,
+                                              status: "not_found",
+                                              url: ftrLink.url,
+                                          }
+                                        : ftrLink
+                                )
+                            );
+                        else
+                            showNotification({
+                                color: "red",
+                                title: "發生錯誤",
+                                message:
+                                    "於預覽連結時發生錯誤，請檢查連結是否正確",
+                            });
+                    });
+            }
+        });
+    };
+
+    useEffect(() => {
+        updateReferenceLinks();
+    }, [referencesLinks]);
+
     return (
-        <div className="relative w-full">
+        <div className="relative w-full pb-20">
             <LoadingOverlay visible={updateArticleMutation.isLoading} />
             <TextInput
                 classNames={{
@@ -200,10 +311,15 @@ const ArticleEditor = ({
                 value={titleText}
                 onChange={(e) => {
                     setTitleText(e.currentTarget.value);
+                    setErrors((cur) => ({
+                        ...cur,
+                        title: null,
+                    }));
                 }}
                 error={errors.title}
             />
             <div className="mt-4 flex w-full flex-col ">
+                <h3 className="mb-2 text-2xl font-semibold">議題內文</h3>
                 {errors.content && (
                     <p className="text-sm text-red-500">{errors.content}</p>
                 )}
@@ -215,7 +331,11 @@ const ArticleEditor = ({
                         <Textarea
                             value={block}
                             onChange={(e) => {
-                                const updateString = e.currentTarget.value;
+                                let updateString = e.currentTarget.value;
+                                updateString = updateString.replaceAll(
+                                    "\n",
+                                    ""
+                                );
                                 setArticleContent((cur) =>
                                     cur.map((block, j) =>
                                         j === i ? updateString : block
@@ -277,7 +397,95 @@ const ArticleEditor = ({
                     <div className="flex-grow border-t border-t-slate-500"></div>
                 </button>
             </div>
-            <Button variant="outline" onClick={submitData}>
+            <div className="my-4 w-full border-b border-b-slate-400" />
+            <div className="w-full">
+                <h3 className="text-2xl font-semibold">參考資料</h3>
+                {errors.refLinks && (
+                    <p className="text-red-500">{errors.refLinks}</p>
+                )}
+                <div className="mt-4 flex w-full flex-col gap-4">
+                    {referencesLinks.map((link, i) => {
+                        return (
+                            <div
+                                key={i}
+                                className="flex w-full items-center gap-2"
+                            >
+                                {link.data ? (
+                                    <ReferenceCard data={link.data} />
+                                ) : link.status === "not_found" ? (
+                                    <p className="text-red-500">
+                                        預覽失敗（404） {link.url}
+                                    </p>
+                                ) : (
+                                    <p className="text-slate-500">{link.url}</p>
+                                )}
+                                <ActionIcon
+                                    onClick={() => {
+                                        setReferenceLinks((cur) =>
+                                            cur.filter(
+                                                (mapLink) =>
+                                                    mapLink.url !== link.url
+                                            )
+                                        );
+                                    }}
+                                >
+                                    <TrashIcon className="w-4" />
+                                </ActionIcon>
+                            </div>
+                        );
+                    })}
+                </div>
+                <div className="mt-2 flex w-full items-center gap-2">
+                    <TextInput
+                        className="flex-grow "
+                        classNames={{ input: "text-slate-500" }}
+                        variant="unstyled"
+                        placeholder="新增連結"
+                        value={addRefLinkInput.text}
+                        onChange={(e) => {
+                            const url = e.currentTarget.value;
+                            setAddRefLinkInput((cur) => ({
+                                ...cur,
+                                text: url,
+                                error: null,
+                            }));
+                        }}
+                        error={addRefLinkInput.error}
+                    />
+                    <ActionIcon
+                        onClick={() => {
+                            if (!isValidHttpUrl(addRefLinkInput.text))
+                                setAddRefLinkInput((cur) => ({
+                                    ...cur,
+                                    error: "請輸入格式正確的URL",
+                                }));
+                            else if (
+                                referencesLinks.some(
+                                    (link) => link.url === addRefLinkInput.text
+                                )
+                            )
+                                setAddRefLinkInput((cur) => ({
+                                    ...cur,
+                                    error: "已將本網址加入參考資料",
+                                }));
+                            else {
+                                setReferenceLinks((cur) => [
+                                    ...cur,
+                                    {
+                                        data: null,
+                                        status: "queued",
+                                        url: addRefLinkInput.text,
+                                    },
+                                ]);
+                                setAddRefLinkInput({ text: "", error: null });
+                            }
+                        }}
+                    >
+                        <PlusIcon className="w-4" />
+                    </ActionIcon>
+                </div>
+            </div>
+            <Button className="mt-6" variant="outline" onClick={submitData}>
                 發布
             </Button>
         </div>
