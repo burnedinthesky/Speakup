9548c9eb5ad9e243b66932bea8f9f2609ae9bed8
ultddeveloper <andrew.mutien@gmail.com>
2022-11-16 22:08:57 +0800

    Integrate Advocate Comments Frontend & Backend

--- src/pages/advocate/comments/index.tsx
+++ src/pages/advocate/comments/index.tsx
@@ -1,32 +1,27 @@
-import { Fragment } from "react";
+import { GetServerSideProps } from "next";
+import { getToken } from "next-auth/jwt";
+import { prisma } from "../../../utils/prisma";
+
 import { AppShell } from "../../../components/Advocate/AppShell";
 import ArticleComments from "../../../components/Advocate/Comments/ArticleComments";
 
-const CommentManagement = () => {
-    const articles: { id: string; title: string }[] = [
-        {
-            id: "clafjfp630000v5bkjjpb77m1",
-            title: "應該以學習歷程作為大學選才管道嗎?",
-        },
-        {
-            id: "clahojc2s0000v5c5bv264ri5",
-            title: "威爾史密斯的奧斯卡獎應該因為閃巴掌被收回嗎?",
-        },
-    ];
+interface FetchedArticles {
+    articles: { id: string; title: string }[];
+}
 
+const CommentManagement = ({ articles }: FetchedArticles) => {
     return (
         <AppShell title="Speakup - 留言管理" highlight="comments">
             <div className="ml-64 px-12 pt-10 pb-40">
                 <div className="mx-auto max-w-6xl">
                     <h1 className="text-3xl font-bold">留言管理</h1>
-                    <div className="mt-10 flex flex-col gap-10">
+                    <div className="mt-4 flex flex-col gap-10">
                         {articles.map((article) => (
-                            <Fragment key={article.id}>
-                                <ArticleComments
-                                    articleId={article.id}
-                                    articleTitle={article.title}
-                                />
-                            </Fragment>
+                            <ArticleComments
+                                key={article.id}
+                                articleId={article.id}
+                                articleTitle={article.title}
+                            />
                         ))}
                     </div>
                 </div>
@@ -36,3 +31,25 @@ const CommentManagement = () => {
 };
 
 export default CommentManagement;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const token = await getToken({ req: context.req });
+
+    if (!token) {
+        return { notFound: true };
+    }
+
+    const data = await prisma.articles.findMany({
+        where: {
+            authorId: token.id,
+        },
+        select: {
+            id: true,
+            title: true,
+        },
+    });
+
+    return {
+        props: { articles: data },
+    };
+};
