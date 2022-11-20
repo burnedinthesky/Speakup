064282cd13d962e83b6b52a1721e9032436e86af
ultddeveloper <andrew.mutien@gmail.com>
2022-11-20 14:23:12 +0800

    Update navigation router to use brief

--- src/components/Navigation/Home/HomeNavCard.tsx
+++ src/components/Navigation/Home/HomeNavCard.tsx
@@ -7,19 +7,18 @@ const HomeNavCard = ({ cardContent }: { cardContent: NavCardData }) => {
         <Link href={`/discussion/${cardContent.id}`}>
             <div
                 className="
-                    flex h-40 w-full 
-                    flex-shrink-0 justify-between overflow-hidden 
-                    border-b-[1px] border-neutral-400 bg-neutral-50 pr-4 md:pr-7 
-                    lg:rounded-2xl lg:border-2 lg:border-primary-800 xl:border-2 
+                    relative flex h-full w-full flex-shrink-0 items-center justify-end overflow-hidden 
+                    border-b border-neutral-400 bg-neutral-50 pr-4 md:pr-7 lg:rounded-2xl 
+                    lg:border-2 lg:border-primary-800 lg:pl-24
                 "
             >
-                <div className="hidden w-[4.5rem] flex-shrink-0 flex-col items-center justify-center bg-primary-800 text-white md:w-24 lg:flex">
+                <div className="absolute top-0 left-0 bottom-0 hidden w-[4.5rem] flex-shrink-0 flex-col items-center justify-center bg-primary-800 text-white md:w-24 lg:flex">
                     {cardContent.tags.map((tag, i) => (
                         <p key={i}>#{tag}</p>
                     ))}
                 </div>
-                <div className="h-full flex-grow px-2 py-3 lg:px-4">
-                    <h3 className="text-lg text-neutral-800 lg:text-xl">
+                <div className="flex h-full flex-grow flex-col justify-center px-2 py-3 lg:px-4">
+                    <h3 className="text-lg text-neutral-800 line-clamp-2 lg:text-xl">
                         {cardContent.title}
                     </h3>
                     <div className="mt-1.5 flex flex-wrap items-center gap-x-4 gap-y-1.5">
@@ -49,7 +48,7 @@ const HomeNavCard = ({ cardContent }: { cardContent: NavCardData }) => {
                             ))}
                         </div>
                     </div>
-                    <p className="mt-2  text-ellipsis text-sm text-neutral-700 line-clamp-3 lg:h-[60px] lg:line-clamp-3 ">
+                    <p className="mt-2 text-ellipsis text-sm text-neutral-700 line-clamp-3 ">
                         {cardContent.brief}
                     </p>
                 </div>
