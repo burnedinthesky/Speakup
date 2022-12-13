7d83321c3d46704a37980500f991d9ce6c231a78
ultddeveloper <andrew.mutien@gmail.com>
2022-12-14 07:36:31 +0800

    Add Daily Cron Job - Clear Article Views

---
+++ src/pages/api/privileged/clear/views.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../../utils/prisma";
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse,
+) {
+	const auth = req.headers.authorization;
+
+	if (auth !== `Bearer ${process.env.ADMIN_API_KEY as string}`) {
+		res.status(401).json({ error: "Unauthorized" });
+		return;
+	}
+
+	await prisma.articleViews.deleteMany();
+
+	res.status(200).json({ success: true });
+}
